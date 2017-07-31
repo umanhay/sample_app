ba5f596cb972e369e3dae693bec19e82b490220d
umanhay <ptown727@yahoo.com>
2017-07-31 16:46:37 -0700

    Update create action of sessions controller, add SessionsHelper

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,13 @@
 module SessionsHelper
+
+  # Logs in the given user
+  # Places temp cookie in user's browser containing hashed user id
+  def log_in(user)
+    session[:user_id] = user.id 
+  end
+
+  # Returns current user if any
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
 end
