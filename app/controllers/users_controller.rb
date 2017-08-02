5330e43c8a782084f7d175ecfe623962627a802c
umanhay <ptown727@yahoo.com>
2017-08-02 13:15:42 -0700

    Add user index and update layout link test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,13 @@
 class UsersController < ApplicationController
   # By default before_action applies to all actions, so use 'only' to restrict to certain actions.
   # Make sure to add tests for this to users_controller_test.rb
-  before_action :logged_in_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update]
   before_action :correct_user, only: [:edit, :update]
   
+  def index
+    @users = User.all 
+  end
+
   def show
     @user = User.find(params[:id])
   end
