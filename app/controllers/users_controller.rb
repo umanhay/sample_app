e08d3ffc15ccc8a06076319394082e3abd5b476f
umanhay <ptown727@yahoo.com>
2017-08-02 12:28:12 -0700

    Add friendly forwarding

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -53,6 +53,8 @@ class UsersController < ApplicationController
     # Confirms a logged-in user.
     def logged_in_user
       unless logged_in?
+        # Sessions helper method.
+        store_location
         flash[:danger] = "Please log in."
         redirect_to login_url
       end
